bb33283706fccec8e482f3261090f2e3c190a1a4
Emily Peregrine <emily.peregrine1@nhs.net>
2022-11-03 14:39:15 +0000

    Refactored test structure

--- test/__init__.py
+++ test/__init__.py
@@ -1,10 +1,8 @@
 """Unit Tests"""
-
-from ast import Import
 import unittest
 
-from .dashboard import *
-from .soil_moisture_sensor import *
+from dashboard.test import *
+from soil_moisture_sensor.test import *
 
 if __name__ == 'main':
     unittest.main()
